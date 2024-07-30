aa023930443994bc471c4216b8e0b293c9f74254
Lucky <Luckysikolia7@gmail.com>
2024-07-30 15:50:53 +0300

    add new If statement files ----------------------------------------- This contains practice on if statmements, if else stamtements, if - else if - else statements and logical operators in c#

---
+++ IfStatementPractice/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatementPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trial();
+            Console.WriteLine();
+            PetsToys();
+            Console.ReadLine();
+        }
+
+        static bool Trial()
+        {
+            bool isMale = true;
+            bool isTall = true;
+
+            //Determine their gender
+            if (isMale)
+            {
+                Console.WriteLine("I am the essence of a man");
+            }
+            else
+            {
+                Console.WriteLine("I am the essence of a woman");
+            }
+
+            //Determine game to play
+            if (isTall)
+            {
+                Console.WriteLine("Sign up for basketball");
+            }
+            else if (isMale)
+            {
+                Console.WriteLine("Sign up for male Swmming team");
+            }
+            else
+            {
+                Console.WriteLine("Sign up for the female Volleyball team");
+            }
+
+            return isMale;
+        }
+
+        //method to check pets and their toy suggestions
+        static bool PetsToys()
+        {
+            bool isPetMale = false;
+            bool isLikeBird = true;
+
+            if (isPetMale)
+            {
+                Console.WriteLine("This is a male cat");
+            }
+            else
+            {
+                Console.WriteLine("This is a female cat");
+            }
+
+            if (isLikeBird)
+            {
+                Console.WriteLine("Get the magic wand toy");
+            }
+            else
+            {
+                Console.WriteLine("Get the moving rat toy");
+            }
+
+            return isLikeBird;
+        }
+
+    }
+}
---
+++ IfStatements2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatements2
+{
+    internal class Program
+    {
+        //The detective examined the crime scene meticulously.If the glass was shattered from the inside, it indicated a 
+        //forced entry.However, if the glass was unbroken but the door was ajar, it suggested a possible insider job. The 
+        //position of the furniture and the absence of valuable items would further solidify the theory.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Help the detective to solve a mystery");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("The detective examined the crime scene meticulously and discovered that.....  ");
+
+            string result = SolveMystery(true, false);
+            Console.Write(result);
+            Console.WriteLine("The position of the furniture and the absence of valuable items would further solidify the theory.");
+
+            Console.ReadLine();
+        }
+
+        static string SolveMystery (bool isGlassShattered, bool isSuspectMale )
+        {
+            //bool isGlassShattered = true;
+            //bool isSuspectMale = true;
+
+            if (isGlassShattered && isSuspectMale)
+            {
+                return "The glass was shattered from the inside, this indicated a forced entry. Judging by the foot steps the suspect is male. ";
+            }
+            //else if(isGlassShattered || isSuspectMale)
+            //{
+            //    return "The glass may have broken but the door was ajar, it suggested a possible insider job. Judging by the foot steps the suspect may be male. ";
+            //}
+            else if (isGlassShattered && !isSuspectMale)
+            {
+                return "The glass was shattered from the inside, this indicated a forced entry. Judging by the foot steps the suspect is female. ";
+            }
+            else if (!isGlassShattered && !isSuspectMale)
+            {
+                return "The glass was not broken but the door was ajar, it suggested a possible insider job. Judging by the foot steps the suspect is female. ";
+            }
+            else
+            {
+                return "We need to search the other rooms for more clues";
+            }
+
+        }
+    }
+}
