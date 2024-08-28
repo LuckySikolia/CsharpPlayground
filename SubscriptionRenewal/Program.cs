3084f45e003c22309558b92229f9b64964f136fb
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-08-28 20:43:36 +0300

    add two susbcription notification code programs

---
+++ SubscriptionRenewal/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscriptionRenewal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* You've been asked to add a feature to your company's software. The feature 
+             * is intended to improve the renewal rate of subscriptions to the software. 
+             * Your task is to display a renewal message when a user logs into the 
+             * software system and is notified their subscription will soon end. 
+             * You'll need to add a couple of decision statements to properly add 
+             * branching logic to the application to satisfy the requirements.*/
+
+            bool isLogIn = true;
+
+            
+
+            //check is user subscription is about to expire notice is 5 days before expiry
+            //set target date
+            DateTime targetDate = new DateTime(2024, 9, 1);
+
+            //set current date
+            DateTime currentDate = DateTime.Now;
+
+            //the days left
+            TimeSpan timeLeft = targetDate - currentDate;
+
+
+            //if user is online
+            if (isLogIn == true && timeLeft <= TimeSpan.FromDays(5))
+            {
+                Console.WriteLine($"Subscription Expiring Soon!\r\n\r\nYour subscription will expire on {targetDate.Date} \r\nRenew now to continue enjoying all the benefits!");
+            }
+
+           
+
+            Console.WriteLine();
+            Console.Write("Viewing days left:  ");
+            Console.WriteLine($"{timeLeft.Days} days.");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SubscriptionRenewalRandomNumber/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscriptionRenewalRandomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+            int daysUntilExpiration = random.Next(12);
+            int discountPercentage = 0;
+
+            Console.WriteLine(daysUntilExpiration);
+            //int totalCost;
+
+            //totalCost = (totalCost - (totalCost * discountPercentage));
+
+            // Your code goes here
+            //if (daysUntilExpiration <= 10)
+            //{
+            //    Console.WriteLine("Your subsciption will expire soon. Renew now!");
+            //}
+            //else if(daysUntilExpiration <= 5)
+            //{
+            //    discountPercentage = 10;
+            //    Console.WriteLine($"Your subsciption will expire in {daysUntilExpiration} days. \n Renew now! and save {discountPercentage}%");
+            //}
+            //else if(daysUntilExpiration <= 1)
+            //{
+            //    Console.WriteLine($"Your subsciption will expires within a day. \n Renew now! and save {discountPercentage}%");
+            //}
+            //else if (daysUntilExpiration == 0)
+            //{
+            //    Console.WriteLine("Your subsciption has expired.");
+            //}
+
+
+
+            if (daysUntilExpiration == 0)
+            {
+                Console.WriteLine("Your subsciption has expired.");
+            }
+            else if (daysUntilExpiration <= 1)
+            {
+                discountPercentage = 20;
+                Console.WriteLine($"Your subsciption will expires within a day. \nRenew now! and save {discountPercentage}%");
+            }
+            else if (daysUntilExpiration <= 5)
+            {
+                discountPercentage = 10;
+                Console.WriteLine($"Your subsciption will expire in {daysUntilExpiration} days. \n Renew now! and save {discountPercentage}%");
+            }
+            else if (daysUntilExpiration <= 10)
+            {
+                Console.WriteLine("Your subsciption will expire soon. Renew now!");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
