7895f981f1bfa6d79510b989b99393bba5602418
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-09-18 23:55:22 +0300

    lear anout array split, join)

---
+++ Discover split() and join()/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discover_split___and_join__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string value = "abc123";
+            char[] valueArray = value.ToCharArray();
+            Array.Reverse(valueArray);
+            // string result = new string(valueArray);
+            //Combine all of the chars into a new comma-separated-value string using Join()
+            string result = String.Join(",", valueArray);
+            Console.WriteLine(result);
+            Console.WriteLine();
+
+            string[] items = result.Split(',');
+            foreach (string item in items)
+            {
+                Console.WriteLine(item);
+            }*/
+
+
+            /* ////challenge 1:Complete a challenge to reverse words in a sentence
+
+             //string pangram = "The quick brown fox jumps over the lazy dog";
+             //string reversedPanagram = new string(pangram.Reverse().ToArray());
+
+             //Console.WriteLine(reversedPanagram);
+            */
+
+
+
+            /*//CHALLENGE 2
+            //Complete a challenge to parse a string of orders, sort the orders and tag possible errors
+
+            string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+            string[] orderStreamArray = orderStream.Split(',');
+            //string[] result = new string[orderStream.Sort()];
+            Console.WriteLine(orderStream);
+            Console.WriteLine();
+
+            //sort the array in ascending order
+            Array.Sort(orderStreamArray);
+
+            foreach (string item in orderStreamArray)
+            {
+                Console.WriteLine(item);
+
+                if (item.ToCharArray().Length != 4)
+                {
+                    Console.WriteLine($"{item} - Error");*//*
+
+
+                }
+            }*/
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Discoversort() reversesort()/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discoversort___reversesort__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pallets = { "B14", "A11", "B12", "A13" };
+
+            Console.WriteLine("Sorted...");
+            Array.Sort(pallets);  //sort the array alphanumerically
+
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Reversed...");
+            Array.Reverse(pallets);
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            /*The Array.Clear() method allows you to remove the contents of specific elements in your array and replace it with the array default value*/
+
+            Array.Clear(pallets, 0, 2);
+            Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+
+            Console.WriteLine("");
+            //In this case, you're resizing the pallets array from four elements to 6. The new elements are added at the end of the
+            //current elements. The two new elements will be null until you assign a value to them.
+            Array.Resize(ref pallets, 6); //the Resize() method passing in the pallets array by reference, using the ref keyword. 
+            Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+            pallets[4] = "C01";
+            pallets[5] = "C02";
+
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
