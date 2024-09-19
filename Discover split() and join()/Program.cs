77e88fea4b4b58074e0a2f7a31210712bff2093b
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-09-19 13:46:27 +0300

    add new project

--- Discover split() and join()/Program.cs
+++ Discover split() and join()/Program.cs
@@ -61,7 +61,18 @@ namespace Discover_split___and_join__
             }*/
 
 
+            string message = "Find what is (inside the parentheses)";
 
+            int openingPosition = message.IndexOf('(');
+            int closingPosition = message.IndexOf(')');
+
+            // Console.WriteLine(openingPosition);
+            // Console.WriteLine(closingPosition);
+
+            openingPosition += 1;
+
+            int length = closingPosition - openingPosition;
+            Console.WriteLine(message.Substring(openingPosition, length));
 
 
 
