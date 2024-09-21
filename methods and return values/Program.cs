2a5ac3a67ab660934991c12fda6d74d63ceb9fe3
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-09-21 15:13:33 +0300

    methods that return values practice

---
+++ methods and return values/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods_and_return_values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
